4969a336e2db134f5255300ecc246550272bb55b
Nicola Giancecchi <nicola@giancecchi.com>
2023-12-21 14:58:15 +0100

    Initial Commit

---
+++ FabSensors2/FabSensors2App.swift
@@ -0,0 +1,17 @@
+//
+//  FabSensors2App.swift
+//  FabSensors2
+//
+//  Created by Nico on 21/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FabSensors2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
